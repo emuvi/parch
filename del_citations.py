02f2d77a5ab36c1c6835ddae484a85d1b5f3ee06
Éverton M. Vieira <emuvi@outlook.com.br>
2025-10-14 12:59:05 -0300

    feat: del citations

---
+++ del_citations.py
@@ -0,0 +1,29 @@
+import re
+import pyperclip
+
+def del_citations(text):
+    text = re.sub(r'\[cite_start\]', '', text)
+    text = re.sub(r'\[cite\:\s+\d+\]', '', text)
+    return text
+
+def main():
+    origin = pyperclip.paste()
+
+    if not origin:
+        print("Clipboard is empty.")
+        return
+
+    print("Origin on Clipboard:")
+    print("---------------------------")
+    print(origin)
+
+    cleaned = del_citations(origin)
+
+    pyperclip.copy(cleaned)
+
+    print("\nCleaned on Clipboard:")
+    print("----------------------------------------------------------------")
+    print(cleaned)
+
+if __name__ == "__main__":
+    main()
