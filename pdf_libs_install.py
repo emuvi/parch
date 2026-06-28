cf49ad285ba0cc87ec04d4e9b63b964c26c4bab6
Éverton M. Vieira <emuvi@outlook.com.br>
2026-06-28 12:46:50 -0300

    feat: installl auxiliary libraries

--- pdf_libs_install.py
+++ pdf_libs_install.py
@@ -4,11 +4,27 @@ import os
 
 
 def install_packages():
-    """Install required Python packages from the internet."""
+    """Install required Python packages and spacy models from the internet."""
     packages = [
-        "lmstd",
+        "PyQt5",
         "PyPDF2",
-        "PyQt5"
+        "lmstd",
+        "click",
+        "spacy",
+        "langdetect"
+    ]
+
+    spacy_models = [
+        "de_core_news_sm",
+        "el_core_news_sm",
+        "en_core_web_sm",
+        "es_core_news_sm",
+        "fr_core_news_sm",
+        "it_core_news_sm",
+        "nl_core_news_sm",
+        "pt_core_news_sm",
+        "ru_core_news_sm",
+        "xx_ent_wiki_sm"
     ]
 
     print("Starting the installation of required libraries...")
@@ -33,8 +49,20 @@ def install_packages():
             print(f"\nERROR: Failed to install '{package}'. Details: {e}")
             return False
 
+    # Download spacy models from the internet
+    for model in spacy_models:
+        try:
+            print(f"\nDownloading spacy model '{model}' from the internet...")
+            subprocess.check_call(
+                [sys.executable, "-m", "spacy", "download", model])
+            print(f"Success: '{model}' downloaded and installed correctly.")
+        except subprocess.CalledProcessError as e:
+            print(
+                f"\nERROR: Failed to download spacy model '{model}'. Details: {e}")
+            return False
+
     print("-" * 50)
-    print("All Python libraries have been successfully installed!")
+    print("All Python libraries and spacy models have been successfully installed!")
     return True
 
 
